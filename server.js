54019c08410e9b3d96dd646331f7d03e7fba1650
thanhdat21293 <thanhdat21293@gmail.com>
2016-11-24 10:26:47 +0700

    Update fixing localhost

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next){
 	if(req.headers['x-forwarded-proto'] === 'https'){
-		req.headers['x-forwarded-proto'] === 'http'
+		res.redirect('http://' + req.hostname + req.url);
 	}else{
 		next();
 	}
